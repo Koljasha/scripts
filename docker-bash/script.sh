901b80912ab8f80bf9194258ee1cd06c28f23017
Koljasha <koljasha@mail.ru>
2025-04-28 23:24:53 +0300

    fix docker-bash

--- docker-bash/script.sh
+++ docker-bash/script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #
-# печатаем дату в файл лога
+# Печатаем дату в файл лога и выводим её в стандартный поток вывода
 #
 
 SCRIPT=$(readlink -f "$0")
@@ -10,9 +10,16 @@ SCRIPTNAME=$(basename "$0")
 
 logs_dir="$SCRIPTPATH/logs"
 if [[ ! -d $logs_dir ]]; then
-    mkdir -p $logs_dir
+    mkdir -p "$logs_dir"
 fi
 log_file="${logs_dir}/${SCRIPTNAME}.log"
 
-date '+%Y-%m-%d %H:%M:%S' >> $log_file
+# Получаем текущую дату и время
+current_date=$(date '+%Y-%m-%d %H:%M:%S')
+
+# Формируем сообщение
+message="${SCRIPTNAME} | ${current_date}"
+
+# Выводим сообщение в файл и стандартный поток вывода
+echo "$message" | tee -a "$log_file"
 
